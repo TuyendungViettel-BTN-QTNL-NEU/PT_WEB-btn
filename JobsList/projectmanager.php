1b31446e64451c10dbfaf4bc7a9bb8996eb4a917
Pahm Anh <lethienmin@gmail.com>
2026-03-29 23:55:49 +0700

    Add files via upload

---
+++ JobsList/projectmanager.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Tuyển dụng Quản lý Dự án</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+
+<!-- HEADER -->
+<?php include "../header.php"; ?>
+
+<!-- THÔNG TIN TUYỂN DỤNG -->
+<div class="container bg-content" style = 'color: #004a99'>
+    <h1>Thông tin vị trí tuyển dụng</h1>
+</div>
+<section class="container" style="padding:60px 0;">
+
+    <div class="job-card" style="padding:40px; text-align:justify; color:#004a99;">
+
+        <h1>Mô tả công việc</h1>
+
+        <h3>1. Xác định công việc</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Tên vị trí: Quản lý dự án</li>
+            <li>Phòng ban: Bộ phận giao nhận</li>
+            <li>Địa điểm làm việc: Tòa nhà Việt Á, số 9 Duy Tân, Phường Cầu Giấy </li>
+            <li>Cấp bậc vị trí: Quản lý</li>
+            <li>Mức lương: Thỏa thuận</li>
+        </ul>
+
+        <h3>2. Trách nhiệm / Nhiệm vụ</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Quản lý vòng đời dự án.</li>
+            <li>Tương tác giữa các yêu cầu và thiết kế giải pháp.</li>
+            <li>Quản lý và hợp tác nhóm.</li>
+            <li>Kiểm soát rủi ro và chất lượng.</li>
+            <li>Tuân thủ và đảm bảo an ninh.</li>
+            <li>Đánh giá và tối ưu hóa dự án.</li>
+        </ul>
+
+        <h3>3. Điều kiện làm việc</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Được cấp đầy đủ công cụ dụng cụ làm việc</li>
+            <li>Làm việc trong môi trường năng động, kỷ luật, an toàn, bảo mật cao.</li>
+            <li>Thời gian làm việc: Thứ 2 - Thứ 6 (từ 08:00 đến 17:00)</li>
+        </ul>
+
+        <h3>4. Quyền lợi</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Làm việc từ xa.</li>
+            <li>Lương cơ bản 100% + thưởng hiệu suất trong thời gian thử việc 2 tháng.</li>
+            <li>Mức đóng bảo hiểm xã hội được tính dựa trên 100% tổng lương.</li>
+            <li>Khám sức khỏe định kỳ hàng năm.</li>
+            <li>Môi trường làm việc năng động, trang phục tự do.</li>
+        </ul>
+
+<!--  Dấu gạch ngang -->
+        <hr style="border:none; height:2px; margin:30px 0; background:#0a66c2;">
+<!--  Bản yêu cầu ứng viên  -->
+        <h1>Yêu cầu ứng viên</h1>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Đã hoàn thành bằng cử nhân trở lên.</li>
+            <li>Đảm bảo sự giao tiếp thông suốt, không rào cản giữa nhân viên tại chỗ và đội ngũ quản lý.</li>
+            <li>Yêu cầu thành thạo tiếng Anh.</li>
+            <li>Thành thạo các phần mềm văn phòng như Excel, Word và PowerPoint.</li>
+            <li>Khả năng chú trọng cao đến từng chi tiết và độ chính xác.</li>
+            <li>Có khả năng làm việc dưới áp lực cao và trong môi trường thay đổi nhanh chóng.</li>
+            <li>Kinh nghiệm tham gia các dự án tương tự là một lợi thế.</li>
+        </ul>
+
+<!--  Dấu gạch ngang -->
+        <hr style="border:none; height:2px; margin:30px 0; background:#0a66c2;">
+<!--  Bản yêu cầu công việc  -->
+        <h1>Yêu cầu công việc</h1>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Đã hoàn thành bằng cử nhân trở lên.</li>
+            <li>Tối thiểu 3 năm kinh nghiệm quản lý dự án, ưu tiên kinh nghiệm quản lý dự án thu thập/chuẩn hóa dữ liệu, bao gồm ít nhất 1 năm kinh nghiệm quản lý nhóm (dẫn dắt nhóm từ 3 người trở lên hoàn thành toàn bộ dự án).</li>
+            <li>Yêu cầu thành thạo tiếng Anh.</li>
+            <li>Ưu tiên ứng viên thành thạo các loại thu thập dữ liệu phổ biến trong các ngành khác nhau, chẳng hạn như LLM, hình ảnh, âm thanh, văn bản, đa phương thức, v.v.</li>
+            <li>Khả năng học hỏi để nhanh chóng nắm vững các công nghệ thu thập dữ liệu mới và kiến ​​thức chuyên ngành.</li>
+            <li>Có khả năng xử lý áp lực từ nhiều dự án song song, các nhu cầu cấp thiết (như tạm thời bổ sung thêm các chiều thu thập dữ liệu) và các vấn đề kỹ thuật, đồng thời duy trì hiệu quả công việc.</li>
+        </ul>
+        <div style="margin-top:40px; text-align:center;">
+            <a href="../index.php" class="btn-primary">Quay lại trang tuyển dụng</a>
+            <a href="../active/formdk.php" class="btn-primary">Ứng tuyển ngay</a>
+
+        </div>
+
+    </div>
+
+</section>
+
+</body>
+</html>

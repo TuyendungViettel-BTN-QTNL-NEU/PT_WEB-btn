229c0882b20950e9ba4bfb90a616bf940aede6d6
Pahm Anh <lethienmin@gmail.com>
2026-03-30 17:29:15 +0700

    Add files via upload

---
+++ JobsList/projectassistant.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Tuyển dụng Trợ lý Dự án</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+
+<!-- HEADER -->
+<?php include "../header.php"; ?>
+
+<!-- THÔNG TIN TUYỂN DỤNG -->
+<div class="container bg-content" style = 'color: #004a99'>
+    <h1>Thông tin vị trí tuyển dụng</h1>
+</div>
+<section class="container" style="padding:60px 0;">
+
+    <div class="job-card" style="padding:40px; text-align:justify; color:#004a99;">
+
+        <h1>Mô tả công việc</h1>
+
+        <h3>1. Xác định công việc</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Tên vị trí: Trợ lý dự án</li>
+            <li>Địa điểm làm việc: Tòa nhà Việt Á, số 9 Duy Tân, Phường Cầu Giấy </li>
+            <li>Cấp bậc vị trí: Trợ lý</li>
+            <li>Mức lương: Từ 16 triệu</li>
+        </ul>
+
+        <h3>2. Trách nhiệm / Nhiệm vụ</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Thực hiện các nhiệm vụ hành chính bằng cách thu thập dữ liệu, phân loại, lưu trữ và gửi hồ sơ dự án cho đúng người phụ trách, công nhân và các bên liên quan.</li>
+            <li>Giám sát các dự án đang triển khai và đảm bảo rằng tất cả các vấn đề liên quan đến dự án được làm rõ và hoàn tất để tránh nhầm lẫn và gián đoạn trong quá trình thực hiện.</li>
+            <li>Ghi lại tất cả các hạng mục dự án và đảm bảo chúng được chuyển giao đầy đủ đến các bộ phận khác nhau.</li>
+            <li>Tham dự các cuộc họp với nhóm và chú ý lắng nghe xuyên suốt để nắm bắt chi tiết các vấn đề được thảo luận trong cuộc họp và có hành động phù hợp.</li>
+        </ul>
+
+        <h3>3. Điều kiện làm việc</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Được cấp đầy đủ công cụ dụng cụ làm việc</li>
+            <li>Làm việc trong môi trường năng động, kỷ luật, an toàn, bảo mật cao.</li>
+            <li>Thời gian làm việc: Thứ 2 - Thứ 6 (từ 08:00 đến 17:00)</li>
+        </ul>
+
+        <h3>4. Quyền lợi</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Làm việc từ xa.</li>
+            <li>Lương cơ bản 100% + thưởng hiệu suất trong thời gian thử việc 2 tháng.</li>
+            <li>Mức đóng bảo hiểm xã hội được tính dựa trên 100% tổng lương.</li>
+            <li>Khám sức khỏe định kỳ hàng năm.</li>
+            <li>Môi trường làm việc năng động, trang phục tự do.</li>
+        </ul>
+
+<!--  Dấu gạch ngang -->
+        <hr style="border:none; height:2px; margin:30px 0; background:#0a66c2;">
+<!--  Bản yêu cầu ứng viên  -->
+        <h1>Yêu cầu ứng viên</h1>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Đã hoàn thành bằng cử nhân trở lên.</li>
+            <li>Đảm bảo sự giao tiếp thông suốt, không rào cản giữa nhân viên tại chỗ và đội ngũ quản lý.</li>
+            <li>Yêu cầu thành thạo tiếng Anh – Trình độ tối thiểu C1 ở 4 kỹ năng (bắt buộc), khả năng giao tiếp tiếng Trung (ưu tiên).</li>
+            <li>Ưu tiên ứng viên có kinh nghiệm quản lý nhóm.</li>
+            <li>Có khả năng giao tiếp tốt, tinh thần làm việc nhóm cao và ý thức trách nhiệm mạnh mẽ.</li>
+            <li>Thành thạo các phần mềm văn phòng như Excel, Word và PowerPoint.</li>
+            <li>Khả năng chú trọng cao đến từng chi tiết và độ chính xác.</li>
+            <li>Có khả năng làm việc dưới áp lực cao và trong môi trường thay đổi nhanh chóng.</li>
+            <li>Kinh nghiệm tham gia các dự án tương tự là một lợi thế.</li>
+        </ul>
+
+        <div style="margin-top:40px; text-align:center;">
+            <a href="../index.php" class="btn-primary">Quay lại trang tuyển dụng</a>
+            <a href="../active/formdk.php" class="btn-primary">Ứng tuyển ngay</a>
+
+        </div>
+
+    </div>
+
+</section>
+
+</body>
+</html>

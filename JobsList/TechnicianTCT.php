4e0ce757fcc6a0bcb60b04d6e06c731aed49ac88
nockdiep <dieptramchitri2004@gmail.com>
2026-03-30 14:25:50 +0700

    Create TechnicianTCT.php

---
+++ JobsList/TechnicianTCT.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Tuyển dụng nhân viên Kỹ Thuật</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+
+<!-- HEADER -->
+<?php include "../header.php"; ?>
+
+<!-- THÔNG TIN TUYỂN DỤNG -->
+<div class="container bg-content" style = 'color: #004a99'>
+    <h1>Thông tin vị trí tuyển dụng</h1>
+</div>
+<section class="container" style="padding:60px 0;">
+
+    <div class="job-card" style="padding:40px; text-align:justify; color:#004a99;">
+
+        <h1>Mô tả công việc</h1>
+
+        <h3>1. Xác định công việc</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Tên vị trí: Nhân viên Kỹ Thuật</li>
+            <li>Phòng ban: Bộ phận Kỹ Thuật</li>
+            <li>Địa điểm làm việc: CÔNG TY TNHH TỰ ĐỘNG HÓA TCT </li>
+            <li>Cấp bậc vị trí: Nhân viên.</li>
+            <li>Người quản lý: Trưởng nhóm.</li>
+            <li>Mức lương: 12 - 18 triệu.</li>
+        </ul>
+
+        <h3>2. Trách nhiệm / Nhiệm vụ</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Lắp đặt và triển khai hệ thống: PLC, Cảm biến, biến tần, Camera vision ,...</li>
+            <li>Lập trình và cấu hình hệ thống: PLC,HMI, ...</li>
+            <li>Bảo trì và sửa chữa hệ thống, thiết bị</li>
+            <li>Tư vấn và hỗ trợ kỹ thuật</li>
+            <li>Nghiên cứu và phát triển</li>
+            <li>Báo cáo và phối hợp công việc</li>
+            <li>Thực hiện các nhiệm vụ khác theo phân công.</li>
+        </ul>
+
+        <h3>3. Điều kiện làm việc</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Được cấp đầy đủ công cụ dụng cụ làm việc</li>
+            <li>Làm việc trong môi trường kỷ luật, an toàn, bảo mật cao.</li>
+            <li>Thời gian làm việc theo quy định của đơn vị, có thể linh hoạt.</li>
+        </ul>
+
+        <h3>4. Quyền lợi</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Mức lương: 12 triệu trở lên/tháng, có lương cứng</li>
+            <li>Được đóng đầy đủ về các chế độ bảo hiểm xã hội, bảo hiểm y tế</li>
+            <li>Hàng tuần, công ty tổ chức buổi Seminar trao đổi, chia sẻ về các công nghệ mới nhất; là cơ hội để các thành viên học hỏi lẫn nhau.</li>
+            <li>Môi trường làm việc thoải mái, tự do phát huy.</li>
+        </ul>
+
+        <h3>5. Quyền hạn</h3>
+        <ul style="padding-left:25px; line-height:1.6;">
+            <li>Quyền sử dụng trang thiết bị</li>
+            <li>Đề xuất & phản hồi các vấn đề phát sinh</li>
+        </ul>
+<!--  Dấu gạch ngang -->
+        <hr style="border:none; height:2px; margin:30px 0; background:#0a66c2;">
+<!--  Bản yêu cầu công việc  -->
+        <h1>Yêu cầu công việc</h1>
+        <h3>1. Yêu cầu về đào tạo/bằng cấp/chứng chỉ</h3>
+        <ul style="padding-left:20px; line-height:1.6;">
+            <li>Tốt nghiệp Trung cấp / Cao đẳng / Đại học các chuyên ngành:
+                <ul>
+                    <li>Tự động hóa</li>
+                    <li>Điện-Điện tử</li>
+                    <li>Cơ điện tử</li>
+                    <li>Công nghệ thông tin</li>
+                </ul>
+            </li>
+        </ul>
+
+        <h3>2. Yêu cầu về kiến thức, kỹ năng</h3>
+        <ul style="padding-left:20px; line-height:1.6;">
+            <li>Hiểu biết về:
+              <ul>
+                <li>PLC, HMI, biến tần, servo</li>
+                <li>Hệ thống cảm biến, camera vision</li>
+                <li>Mạch điện công nghiệp, điều khiển tự động</li>
+              </ul>
+            </li>
+            <li>Có kiến thức lập trình cơ bản (C/C++, Ladder, hoặc tương đương)</li>
+            <li>Có kiến thức về thiết kế và vận hành hệ thống HMI, SCADA</li>
+        </ul>
+        
+        <h3>3. Yêu cầu về kinh nghiệm</h3>
+        <ul style="padding-left:20px; line-height:1.6;">
+            <li>Ưu tiên ứng viên có kinh nghiệm thực tập hoặc làm việc trong lĩnh vực tự động hóa</li>
+        </ul>
+        
+        <h3>4. Yêu cầu về phẩm chất và thái độ làm việc</h3>
+        <ul style="padding-left:20px; line-height:1.6;">
+            <li>Lý lịch cá nhân: Lý lịch rõ ràng, không có tiền án, tiền sự, không nghiện hút chất bị cấm, không vi phạm pháp luật.</li>
+            <li>Trung thực, cẩn thận, có tinh thần trách nhiệm cao.</li>
+            <li>Tuân thủ quy trình và quy định của tổ chức.</li>
+            <li>Có ý thức bảo mật thông tin.</li>
+        </ul>
+
+        <div style="margin-top:40px; text-align:center;">
+            <a href="../index.php" class="btn-primary">Quay lại trang tuyển dụng</a>
+            <a href="../active/formdk.php" class="btn-primary">Ứng tuyển ngay</a>
+
+        </div>
+
+    </div>
+
+</section>
+
+</body>
+</html>
